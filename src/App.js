8dc0299154bd0f75e48a26ae7e6cb850c523c15c
TrieuBui1006 <buiductrieu1006@gmail.com>
2020-07-17 23:47:00 +0200

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Components
+import { Cards, Chart, CountryPicker } from './components'
+
+//Styles
+import Styles from './App.module.css'
+
+//functions
+import { fetchData } from './api'
+
+class App extends Component {
+  state = {
+    data: {},
+  }
+
+  async componentDidMount() {
+    const fetchedData = await fetchData()
+
+    this.setState({ data: fetchedData })
+  }
+
+  render() {
+    const { data } = this.state
+
+    return (
+      <div className={Styles.container}>
+        <Cards data={data} />
+        <Chart />
+        <CountryPicker />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
